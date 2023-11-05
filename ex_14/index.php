d43c4f5571773f509332372e813f77f374cf87c3
Alisson Sommer <alissonsommer1@gmail.com>
2023-11-05 17:44:28 -0300

    Update index.php

--- ex_14/index.php
+++ ex_14/index.php
@@ -1,17 +1,29 @@
 <?php
   require './model/Usuario.php';
 
-  $oUsuario = new app\model\Usuario("João", "joao123", "senha123");
+  require './lib/HtmlTable.php';
+  require './lib/HtmlInput.php';
 
-  $oSessao  = new app\model\Sessao();
-  $oSessao->iniciaSessao($oUsuario);
-  $oSessao->setDataHoraInicio(new DateTime());
-  $oSessao->setDataHoraUltimoAcesso(new DateTime());
+  // $oUsuario = new app\model\Usuario("João", "joao123", "senha123");
 
-  echo "Usuário: " . $oSessao->getUsuarioSessao()->getUserName() . "<br>";
-  echo "Login: " . $oSessao->getUsuarioSessao()->getUserLogin() . "<br>";
-  echo "Status da sessão: " . $oSessao->getStatus() . "<br>";
+  // $oSessao  = new app\model\Sessao();
+  // $oSessao->iniciaSessao($oUsuario);
+  // $oSessao->setDataHoraInicio(new DateTime());
+  // $oSessao->setDataHoraUltimoAcesso(new DateTime());
 
-  $oSessao->finalizaSessao();
-  echo "Status da sessão após finalização: " . $oSessao->getStatus() . "<br>";
+  // echo "Usuário: " . $oSessao->getUsuarioSessao()->getUserName() . "<br>";
+  // echo "Login: " . $oSessao->getUsuarioSessao()->getUserLogin() . "<br>";
+  // echo "Status da sessão: " . $oSessao->getStatus() . "<br>";
+
+  // $oSessao->finalizaSessao();
+  // echo "Status da sessão após finalização: " . $oSessao->getStatus() . "<br>";
+
+  $table = new \app\lib\HtmlTable('table1', 'table_id', 'border: 1px solid black;', 2, 2);
+  $table->addLinha(['Nome', 'Idade']);
+  $table->addLinha(['irineu', '30']);
+
+  $input = new \app\lib\HtmlInput('input1', 'input_id', 'width: 200px;', 'Nome:', 'Digite Seu nome...', false);
+
+  echo $table->renderHtml();
+  echo $input->renderHtml();
 ?>
